941ecaed66067f2b3a62590bb15952ae2788396d
Samiya Jabbar <samiya.programming@gmail.com>
2021-02-02 13:52:38 +0500

    changed with get variable from url

--- app.py
+++ app.py
@@ -2,30 +2,30 @@ from flask import Flask, jsonify , request
 
 app = Flask(__name__)
 
-@app.route('/')
+number= 2
+@app.route('/a')
 def hello():
-    return "<h1>HELLO, MATHEMATICS<h1>"
+    return "<h1>WELCOME TO THE WORLD OF MATHEMATICS<h1>"
 
-@app.route('/add/2/2')
-def add():
-    a = 2+2
-    return str(a)
+@app.route('/add/<int:usernumber1>/<int:usernumber2>')
+def add(usernumber1, usernumber2):
+    a= usernumber1+usernumber2
+    return "The sum of these two numbers is %s" % a
 
-@app.route('/multiply/2/2')
-def multiply():
-    a= 2*2
-    return str(a)
+@app.route('/multiply/<int:usernumber1>/<int:usernumber2>')
+def multiply(usernumber1, usernumber2):
+    a= usernumber1*usernumber2
+    return "The product of these two numbers is %s" % a
 
-@app.route('/divide/2/2')
-def divide():
-    a= 2/2
-    a=int(a)
-    return str(a)
+@app.route('/divide/<int:usernumber1>/<int:usernumber2>')
+def divide(usernumber1, usernumber2):
+    a= usernumber1/usernumber2
+    return "The result of these two numbers is %s" % a
 
-@app.route('/sub/2/2')
-def sub():
-    a= 2-2
-    return str(a)
+@app.route('/sub/<int:usernumber1>/<int:usernumber2>')
+def sub(usernumber1, usernumber2):
+    a= usernumber1-usernumber2
+    return "The difference of these two numbers is %s" % a
 
 if __name__=="__main__":
     app.run()
